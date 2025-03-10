b1e4ab796cfa1dc0ea937d2fdecf6c90accab62b
Skordio <skordion@hotmail.com>
2025-03-10 17:59:25 -0400

    clean up compiler warnings for script

--- daily_question/3306_count_substrings_containing/prog.c
+++ daily_question/3306_count_substrings_containing/prog.c
@@ -1,3 +1,5 @@
+// Answer ---------------------------------------------------------------------
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
@@ -6,7 +8,7 @@ typedef struct {
     bool a, e, i, o, u;
     int consonants;
     int (*check)(struct CharTracker* self, int k);
-    void (*read)(struct CharTracker *self, char c);
+    void (*read)(struct CharTracker* self, char c);
 } CharTracker;
 
 // Returns 0 for substring not found yet, 1 for substring found, 2 if subtring cannot be found
@@ -86,33 +88,50 @@ long long countOfSubstrings(char* word, int k)
     int substr_count = 0;
 
     for (int i = 0; i < arr_length; i++) {
-        if(validSubstringExists(&word, k, i, arr_length)) {
+        if(validSubstringExists(word, k, i, arr_length)) {
             substr_count++;
         }
     }
+
+    return substr_count;
 }
 
-int main() {
+// Tests ----------------------------------------------------------------------
+
+void test_1() {
     char word[6] = "aeioqq";
     int k = 1;
 
-    long count = countOfSubstrings(&word, k);
+    long count = countOfSubstrings(word, k);
 
-    printf("Example 1 answer: %d", count);
+    printf("Example 1 answer: %ld\n", count);
+}
+
+void test_2() {
     
     char word[5] = "aeiou";
     int k = 0;
 
-    long count = countOfSubstrings(&word, k);
+    long count = countOfSubstrings(word, k);
 
-    printf("Example 2 answer: %d", count);
-    
+    printf("Example 2 answer: %ld\n", count);
+}
+
+void test_3() {
     char word[14] = "ieaouqqieaouqq";
     int k = 1;
 
-    long count = countOfSubstrings(&word, k);
+    long count = countOfSubstrings(word, k);
 
-    printf("Example 3 answer: %d", count);
+    printf("Example 3 answer: %ld\n", count);
+}
+
+// Main -----------------------------------------------------------------------
+
+int main() {
+    test_1();
+    test_2();
+    test_3();
 
     return 0;
 }
