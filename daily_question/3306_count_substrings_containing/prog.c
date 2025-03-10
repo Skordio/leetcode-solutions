8608593b09fc3ae288145de900253fd742d7d177
Skordio <skordion@hotmail.com>
2025-03-10 14:17:48 -0400

    work on solution, not finished initial implementation yet

--- daily_question/3306_count_substrings_containing/prog.c
+++ daily_question/3306_count_substrings_containing/prog.c
@@ -1,7 +1,53 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
-long long countOfSubstrings(char* word, int k) {
+typedef struct {
+    bool a, e, i, o, u;
+    int consonants;
+    int (*check)(struct CharTracker* self, int k);
+} CharTracker;
+
+// Returns 0 for substring not found yet, 1 for substring found, 2 for subtring cannot be found, k is larger than acceptable consonants
+int charTrackerCheck(CharTracker* t, int k)
+{
+    if(t->consonants < k) {
+        return 0;
+    } else if(t->consonants > k) {
+        return 2;
+    }
+
+    if(t->a && t->e && t->i && t->o && t->u) {
+        return 1;
+    }
+}
+
+void initializeCharTracker(CharTracker* t)
+{
+    t->a = t->e = t->i = t->o = t->u = false;
+    t->consonants = 0;
     
+}
+
+
+bool substringCheck(char* word, int k, int index, int arr_length) 
+{
+    CharTracker tracker = {false, false, false, false, false, 0, charTrackerCheck};
+
+
+
+
+    return 0;
+}
+
+long long countOfSubstrings(char* word, int k) 
+{
+    int arr_length = strlen(word);
+    int substr_count = 0;
+
+    for (int i = 0; i < arr_length; i++) {
+
+    }
 }
 
 int main() {
