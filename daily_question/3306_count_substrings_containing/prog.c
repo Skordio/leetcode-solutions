4719cec3957666125b5e0c619eae939d1ca3728e
Skordio <skordion@hotmail.com>
2025-03-10 17:28:57 -0400

    finish initial implementation for daily 3306

--- daily_question/3306_count_substrings_containing/prog.c
+++ daily_question/3306_count_substrings_containing/prog.c
@@ -6,9 +6,10 @@ typedef struct {
     bool a, e, i, o, u;
     int consonants;
     int (*check)(struct CharTracker* self, int k);
+    void (*read)(struct CharTracker *self, char c);
 } CharTracker;
 
-// Returns 0 for substring not found yet, 1 for substring found, 2 for subtring cannot be found, k is larger than acceptable consonants
+// Returns 0 for substring not found yet, 1 for substring found, 2 if subtring cannot be found
 int charTrackerCheck(CharTracker* t, int k)
 {
     if(t->consonants < k) {
@@ -22,22 +23,61 @@ int charTrackerCheck(CharTracker* t, int k)
     }
 }
 
-void initializeCharTracker(CharTracker* t)
+void charTrackerReadLetter(CharTracker* t, char c)
 {
-    t->a = t->e = t->i = t->o = t->u = false;
-    t->consonants = 0;
-    t->check = charTrackerCheck;
+    switch(c) 
+    {
+        case 'a':
+            t->a = true;
+            return;
+        case 'e':
+            t->e = true;
+            return;
+        case 'i':
+            t->i = true;
+            return;
+        case 'o':
+            t->o = true;
+            return;
+        case 'u':
+            t->u = true;
+            return;
+        default:
+            break;
+    }
+
+    if((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) 
+    {
+        t->consonants++;
+    }
 }
 
-
-bool substringCheck(char* word, int k, int index, int arr_length) 
+CharTracker createCharTracker()
 {
-    CharTracker tracker = {false, false, false, false, false, 0, charTrackerCheck};
+    CharTracker t = {false, false, false, false, false, 0, charTrackerCheck, charTrackerReadLetter};
+    return t;
+}
 
+bool validSubstringExists(char* word, int k, int index, int arr_length) 
+{
+    CharTracker tracker = createCharTracker();
 
+    while(index < arr_length)
+    {
+        tracker.read(&tracker, word[index]);
 
+        int check_val = tracker.check(&tracker, k);
 
-    return 0;
+        if(check_val == 1) {
+            return true;
+        } else if (check_val == 2) {
+            return false;
+        }
+
+        index++;
+    }
+
+    return false;
 }
 
 long long countOfSubstrings(char* word, int k) 
@@ -46,11 +86,33 @@ long long countOfSubstrings(char* word, int k)
     int substr_count = 0;
 
     for (int i = 0; i < arr_length; i++) {
-
+        if(validSubstringExists(&word, k, i, arr_length)) {
+            substr_count++;
+        }
     }
 }
 
 int main() {
+    char word[6] = "aeioqq";
+    int k = 1;
+
+    long count = countOfSubstrings(&word, k);
+
+    printf("Example 1 answer: %d", count);
+    
+    char word[5] = "aeiou";
+    int k = 0;
+
+    long count = countOfSubstrings(&word, k);
+
+    printf("Example 2 answer: %d", count);
+    
+    char word[14] = "ieaouqqieaouqq";
+    int k = 1;
+
+    long count = countOfSubstrings(&word, k);
+
+    printf("Example 3 answer: %d", count);
 
     return 0;
 }
