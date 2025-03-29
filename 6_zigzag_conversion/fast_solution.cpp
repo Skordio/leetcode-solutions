b35a98fd0edf5176a99f21cd3fabbd2d33a4222d
Skordio <skordion@hotmail.com>
2025-03-29 13:30:33 -0400

    add edge cases to zigzag fast solution; slight cleanup also

--- 6_zigzag_conversion/fast_solution.cpp
+++ 6_zigzag_conversion/fast_solution.cpp
@@ -6,13 +6,17 @@ using namespace std;
 class Solution {
     public:
         string convert(string s, int numRows) {
+            if (numRows == 1) {
+                return s;
+            }
+
             vector<int> char_group_indices = {};
 
             int down_group_height = numRows;
             int up_group_height = numRows - 2;
 
             if (s.length() <= numRows) {
-                char_group_indices.push_back(0);
+                return s;
             } else {
                 int index = 0;
                 while (index < s.length()) {
@@ -31,45 +35,33 @@ class Solution {
                 }
             }
 
-
-            // rowNum = 0 -> s[char_group_indices[even indexes]]
-
-            // rowNum = 1 through second to last rowNum - 
-            //      s[char_group_indices[even indexes] + rowNum]
-            //      s[char_group_indices[odd indexes] + up_group_height - (rowNum)]
-
             string out_string = "";
 
             for (int i = 0; i < char_group_indices.size(); i++)
             {
-                if (i % 2 != 0)
-                    continue;
-                out_string += s[char_group_indices[i]];
+                if (i % 2 == 0 && char_group_indices[i] < s.size())
+                    out_string += s[char_group_indices[i]];
             }
 
             for (int rowCount = 1; rowCount < (numRows - 1); rowCount++) {
                 for (int i = 0; i < char_group_indices.size(); i++) {
-                    if (i % 2 == 0)
-                    {
-                        int char_index = char_group_indices[i] + rowCount;
-                        if (char_index < s.size())
-                            out_string += s[char_index];
-                    }
-                    else
-                    {
-                        int char_index = char_group_indices[i] + up_group_height - (rowCount);
-                        if (char_index < s.size())
-                            out_string += s[char_index];
-                    }
+                    int char_index;
 
+                    if (i % 2 == 0)
+                        char_index = char_group_indices[i] + rowCount;
+                    else
+                        char_index = char_group_indices[i] + up_group_height - (rowCount);
+
+                    if (char_index < s.size())
+                        out_string += s[char_index];
                 }
             }
 
             for (int i = 0; i < char_group_indices.size(); i++) 
             {
-                if (i % 2 != 0)
-                    continue;
-                out_string += s[char_group_indices[i] + (numRows-1)];
+                int char_index = char_group_indices[i] + (numRows - 1);
+                if (i % 2 == 0 && char_index < s.size())
+                    out_string += s[char_index];
             }
 
             return out_string;
