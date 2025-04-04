822c8684fccf93ca236661bb99a1eaf0e7ca67c6
Skordio <skordion@hotmail.com>
2025-04-04 16:00:06 -0400

    go over each question and print out its values

--- daily_question/2140_solving_with_brainpower/solution.cpp
+++ daily_question/2140_solving_with_brainpower/solution.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
@@ -7,11 +8,24 @@ class Solution
     public:
         long long mostPoints(vector<vector<int>>& questions)
         {
-            
+            for (vector<int> question : questions)
+            {
+                int score = question[0];
+                int skips = question[1];
+                cout << "[" << score << ", " << skips << "]" << endl;
+            }
+
+            return 0;
         }
 };
 
 int main() 
 {
+    Solution sol;
 
+    vector<vector<int>> test1 = {{3, 2}, {4, 3}, {4, 4}, {2, 5}};
+
+    sol.mostPoints(test1);
+
+    return 0;
 }
