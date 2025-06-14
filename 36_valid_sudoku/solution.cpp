37d79816b8abfc5076b014511193f26f751935bf
Skordio <skordion@hotmail.com>
2025-06-13 20:53:14 -0400

    start implementing solution for sudoku

--- 36_valid_sudoku/solution.cpp
+++ 36_valid_sudoku/solution.cpp
@@ -6,6 +6,38 @@ using namespace std;
 class Solution {
     public:
         bool isValidSudoku(vector<vector<char>>& board) {
-            
+            // Columns
+            for (int col = 0; col < 9; col++) {
+                int appearances[9] = {0,0,0,0,0,0,0,0,0};
+
+                for (int row = 0; row < 9; row++) {
+                    if (board[row][col] == '.')
+                        continue;
+                    int number = (board[row][col] - '0') - 1;
+                    appearances[number]++;
+                    cout << (board[row][col] - '0') << endl;
+                    if (appearances[number] > 1) {
+                        return false;
+                    }
+                }
+            }
+
+            //Rows
+            for (int row = 0; row < 9; row++) {
+                int appearances[9] = {0,0,0,0,0,0,0,0,0};
+
+                for (int col = 0; col < 9; col++) {
+                    if (board[row][col] == '.')
+                        continue;
+                    int number = (board[row][col] - '0') - 1;
+                    appearances[number]++;
+                    cout << (board[row][col] - '0') << endl;
+                    if (appearances[number] > 1) {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
         }
     };
