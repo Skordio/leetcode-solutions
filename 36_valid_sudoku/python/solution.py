57610ebeb84a632006b9e1461037d126fc4e7043
Skordio <skordion@hotmail.com>
2025-09-15 17:50:06 -0400

    work on valid sudoku

--- 36_valid_sudoku/python/solution.py
+++ 36_valid_sudoku/python/solution.py
@@ -2,6 +2,27 @@ from typing import List
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        
-        pass
+        # empty squares are denoted by periods
+
+        squares = [[[], [], []], [[], [], []], [[], [], []]]
+        rows = [[] for num in range(0,9)]
+        cols = [[] for num in range(0,9)]
+
+        # check rows
+        for row in range(0, 9):
+            for col in range(0, 9):
+                num = board[row][col]
+
+                if rows[row].count(num) != 0:
+                    return False
+                elif cols[col].count(num) != 0:
+                    return False
+                elif squares[row // 3][col // 3].count(num) != 0:
+                    return False
+                
+                rows[row].append(num)
+                cols[col].append(num)
+                squares[row // 3][col // 3].append(num)
+
+        return True
 
--- 36_valid_sudoku/python/test.py
+++ 36_valid_sudoku/python/test.py
@@ -1,7 +1,14 @@
 from .solution import Solution
+from typing import List
+
+class Test:
+    board: List[List[str]]
+
+    def __init__(self, board):
+        self.board
 
 def main():
-    
+
     pass
 
 if __name__ == "__main__":
