76ca48df52e0dcd9631865b120be8f67d1f81d59
Skordio <skordion@hotmail.com>
2025-09-19 19:37:38 -0400

    first attempt at 3408, failing on time

--- daily_question/3408_design_task_man/sol.py
+++ daily_question/3408_design_task_man/sol.py
@@ -10,7 +10,7 @@ class TaskManager:
         self._sort_by_priority()
     
     def _sort_by_priority(self):
-        self.tasks.sort(key=lambda x: x[2])
+        self.tasks.sort(key=lambda x: (x[2], x[1]), reverse=True)
         self._index_all_tasks()
 
     def _index_all_tasks(self):
@@ -19,24 +19,44 @@ class TaskManager:
             self.indexForId[task[1]] = i
 
     def add(self, userId: int, taskId: int, priority: int) -> None:
-        self.indexForId[taskId] = len(self.tasks)
-        self.tasks.append[[userId, taskId, priority]]
-        self._sort_by_priority()
+        insertIndex = len(self.tasks)
+        if len(self.tasks):
+            for i, task in enumerate(self.tasks):
+                if task[2] > priority or (task[2] >= priority and task[1] > taskId):
+                    continue
+                insertIndex = i
+                break
+            for i in range(insertIndex, len(self.tasks)):
+                self.indexForId[self.tasks[i][1]] += 1
+        else:
+            insertIndex = 0
+        self.tasks.insert(insertIndex, [userId, taskId, priority])
+        self.indexForId[taskId] = insertIndex
+
 
     def edit(self, taskId: int, newPriority: int) -> None:
         task = self.tasks[self.indexForId[taskId]]
         edited = [task[0], task[1], newPriority]
-        self.tasks[self.indexForId[taskId]] = edited
+        self.rmv(taskId)
+        self.add(*edited)
 
     def rmv(self, taskId: int) -> None:
-        self.tasks.pop(self.indexForId[taskId])
-        self.indexForId.pop(taskId)
+        index = self.indexForId[taskId]
+        self.tasks.pop(index)
+        if len(self.tasks):
+            for i in range(index, len(self.tasks)):
+                self.indexForId[self.tasks[i][1]] -= 1
+        else:
+            self.indexForId = {}
 
     def execTop(self) -> int:
-        pass
-
-    
-
+        try:
+            task = self.tasks[0]
+        except:
+            return -1
+        self.rmv(task[1])
+        return task[0]
+        
 
 # Your TaskManager object will be instantiated and called as such:
 # obj = TaskManager(tasks)
--- daily_question/3408_design_task_man/test.py
+++ daily_question/3408_design_task_man/test.py
@@ -2,7 +2,43 @@ from .sol import TaskManager
 
 tasks = [[1, 101, 10], [2, 102, 20], [3, 103, 15]]
 obj = TaskManager(tasks)
-# obj.add(4,104,5)
-# obj.edit(102,8)
-# obj.rmv(101)
-# param_4 = obj.execTop()
+print(None)
+print(obj.add(4,104,5))
+print(obj.edit(102,8))
+print(obj.execTop())
+print(obj.rmv(101))
+print(obj.add(5, 105, 15))
+print(obj.execTop())
+
+print("\n")
+
+tasks = [[1,101,8],[2,102,20],[3,103,5]]
+obj = TaskManager(tasks)
+print(None)
+print(obj.add(4, 104, 5))
+print(obj.edit(102, 9))
+print(obj.execTop())
+print(obj.rmv(101))
+print(obj.add(50, 101, 8))
+print(obj.execTop())
+
+print("\n")
+
+tasks = [[2,26,15],[6,1,19]]
+obj = TaskManager(tasks)
+print(None)
+print(obj.edit(26, 15))
+print(obj.execTop())
+
+
+print("\n")
+
+tasks = [[10,12,4],[2,15,2],[9,1,23],[5,21,2],[6,19,26],[10,7,47],[4,28,14],[1,10,6],[8,0,15]]
+obj = TaskManager(tasks)
+print(None)
+print(obj.add(6, 3, 0))
+print(obj.execTop())
+print(obj.add(7, 27, 46))
+print(obj.add(6, 20, 46))
+print(obj.rmv(21))
+print(obj.execTop())
